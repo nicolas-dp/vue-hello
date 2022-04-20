857c13b511a78018b970cd6413ecc56db342a6b3
Nicolas <wellcomenicolas@gmail.com>
2022-04-20 13:09:18 +0200

    Stamp message whit data

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus (fatelo):
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 confermate lettura come al solito e buon divertimento */
+
+
+
+const app = new Vue({
+
+    el: "#app",
+
+    data:{
+        text: "Ciao mamma guarda come mi diverto" //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+    }
+})
