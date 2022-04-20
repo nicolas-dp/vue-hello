6bc151f64dcf8cff1107a1eca93f65cbebe6cd2d
Nicolas <wellcomenicolas@gmail.com>
2022-04-20 16:25:46 +0200

    Final

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
 Bonus (fatelo):
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
-confermate lettura come al solito e buon divertimento */
+ */
 
 
 
@@ -13,6 +13,8 @@ const app = new Vue({
 
     data:{
         text: "Ciao mamma guarda come mi diverto", //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+        /* BONUS */
         image: "https://picsum.photos/200"
     }
 })
